3bc3af6a41c9dd9da3b9c8431e3412775350aa84
rubeans <rubensglf@gmail.com>
2022-10-11 02:03:28 -0300

    change design colors, add buttons on header to choose differents grid size and add a footer

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Draw pixels.
 ##
-Default grid is 16x16 but you can change it between 2(min) and 100(max).
+Default grid is 16x16, which is the minimum but you can change it until 128x128, which is the maximum.
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const container = document.querySelector(".container")
+const gridBtn = document.querySelectorAll(".grid-btn")
 
 const createDivs = (cols, rows) => {
     for (let i = 1; i <= (cols * rows); i++) {
@@ -8,5 +9,22 @@ const createDivs = (cols, rows) => {
         container.appendChild(div).classList.add("square")
     }
 }
+const getScale = () => {
+    for (let i = 0; i < gridBtn.length; i++) {
+        gridBtn[i].addEventListener("click", (e) => {
+            // console.log(e.target.attributes.class.nodeValue)
+            if (e.target.attributes.class.nodeValue == "sixteen grid-btn") {
+                createDivs(16, 16)
+            } else if (e.target.attributes.class.nodeValue == "thirty-two grid-btn") {
+                createDivs(32, 32)
+            } else if (e.target.attributes.class.nodeValue == "sixty-four grid-btn") {
+                createDivs(64, 64)
+            } else {
+                createDivs(128, 128)
+            }
+        })
+    }
+}
+getScale()
 
-createDivs(100, 100)
+//TODO - make 16x16 the default scale when load the page
