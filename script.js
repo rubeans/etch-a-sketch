d8915a4dbc49b4510803e6571e1d1b656c84852f
rubeans <rubensglf@gmail.com>
2022-10-12 22:30:04 -0300

    add a new font and update the design, also update js code to when the specific grid is choosen it start to draw

--- script.js
+++ script.js
@@ -1,30 +1,44 @@
 const container = document.querySelector(".container")
 const gridBtn = document.querySelectorAll(".grid-btn")
+const defaultGrid = 16
+const defaultColor = "#000"
+let currentColor;
 
+// CREATE THE GRIDS
 const createDivs = (cols, rows) => {
     for (let i = 1; i <= (cols * rows); i++) {
         const div = document.createElement("div")
         container.style.gridTemplateColumns = `repeat(${cols}, 1fr)`
         container.style.gridTemplateRows = `repeat(${rows}, 1fr)`
-        container.appendChild(div).classList.add("square")
+        container.appendChild(div).classList.add("grid-box")
     }
 }
-const getScale = () => {
-    for (let i = 0; i < gridBtn.length; i++) {
-        gridBtn[i].addEventListener("click", (e) => {
-            // console.log(e.target.attributes.class.nodeValue)
-            if (e.target.attributes.class.nodeValue == "sixteen grid-btn") {
-                createDivs(16, 16)
-            } else if (e.target.attributes.class.nodeValue == "thirty-two grid-btn") {
-                createDivs(32, 32)
-            } else if (e.target.attributes.class.nodeValue == "sixty-four grid-btn") {
-                createDivs(64, 64)
-            } else {
-                createDivs(128, 128)
-            }
-        })
-    }
-}
-getScale()
 
-//TODO - make 16x16 the default scale when load the page
+// REMOVE THE GRIDS
+const removeDivs = () => {
+    // TODO 
+}
+
+// DEFAULT SETTINGS
+const defaultSettings = () => {
+    createDivs(defaultGrid, defaultGrid)
+    //TODO - make 16x16 the default scale when load the page
+    // TODO - NEED TO FINISH
+}
+
+// PEN
+const pen = () => {
+    const gridBox = document.querySelectorAll(".grid-box")
+    gridBox.forEach(box => box.addEventListener("mouseover", () => {
+        currentColor = defaultColor
+        box.style.backgroundColor = `${currentColor}`
+    }))
+}
+
+// CHOOSE THE GRID
+gridBtn.forEach(btn => btn.addEventListener("click", () => {
+    const customGrid = btn.value
+    //TODO removeDivs()
+    createDivs(customGrid, customGrid)
+    pen()
+}))
